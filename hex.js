57b6d4b61c6e882f395bce802d41c85d4ec7f67f
Jhen Nunez <jxelektro@gmail.com>
2022-08-14 17:10:39 -0400

    Comentarios agregados en los archivos JS

--- hex.js
+++ hex.js
@@ -2,17 +2,16 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn")
 const color = document.querySelector(".color")
 
-btn.addEventListener("click", function () {
-  let hexColor = "#";
-  for (let i = 0; i < 6; i++) {
-    hexColor += hex[getRandomNumber()]
+btn.addEventListener("click", function () { // Paso 1. escucha el click del boton
+  let hexColor = "#"; // Paso 2. Inicializa la variable solo con el #
+  for (let i = 0; i < 6; i++) { // Paso 3. le da 6 elementos mas a la variable
+    hexColor += hex[getRandomNumber()] // Paso 5. toma el resultado de getrandom y lo inclutye en cada uno de sus iteraciones 
   }
-  color.textContent = hexColor;
-  document.body.style.backgroundColor = hexColor;
-  console.log(getRandomNumber())
+  color.textContent = hexColor; // Paso 6. pinta el resultado en la pantalla
+  document.body.style.backgroundColor = hexColor; // Paso 7 Pinta el resultado en el body
 });
 
-function getRandomNumber() {
-  return Math.floor(Math.random() * hex.length);
+function getRandomNumber() { 
+  return Math.floor(Math.random() * hex.length); // Paso 4. Crea un numero aleatorio entre 0 y el numero de elementos de hex
 }
 
--- main.js
+++ main.js
@@ -2,14 +2,13 @@ const colors = ["green", "red", "rgba(133,122,200", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-btn.addEventListener("click", function () {
+btn.addEventListener("click", function () { // Paso 1. cuando es pulsado click se inicia
   //get random number btween 0-3,color [3]
-  const randomNumber = getRandomNumber();
-  console.log(randomNumber)
-  document.body.style.backgroundColor = colors[randomNumber]
-  color.textContent = colors[randomNumber]
+  const randomNumber = getRandomNumber(); // Paso 4 Crea la var numero aleatorio con la funcion de abajo
+  document.body.style.backgroundColor = colors[randomNumber] // Paso 5 Le da estilo al fondo del body con el numero aleatorio
+  color.textContent = colors[randomNumber] // paso 6 imprime en pantalla el numero aleatorio
 });
 
-function getRandomNumber(){
-  return Math.floor(Math.random()*colors.length);
+function getRandomNumber(){  // Paso 2 . Seleccionara un numero aleatorio
+  return Math.floor(Math.random()*colors.length); // Paso 3 Numero entero redondeado entre 0 y la cantidad de elementos en colors
 };
